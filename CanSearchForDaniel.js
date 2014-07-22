e41fc6a683d4b014e9a032dcae2999b71f963596
HOMMA Takashi <homma@daniel-soft.com>
2014-07-22 18:29:27 +0900

    検索後のひとつ一つのリンク選ぶ処理を実装する

--- CanSearchForDaniel.js
+++ CanSearchForDaniel.js
@@ -2,6 +2,27 @@ var casper = require("casper").create({
     verbose: true
 });
 
+var count = 0;
+
+var openUrl = function() {
+	this.thenOpen( "https://www.hellowork.go.jp/servicef/" + this.getElementsAttribute('table.sole-small #ID_link', 'href')[count], function() {
+		this.echo( this.getTitle());
+		this.capture('submit' + count + '.png');
+		this.back();
+		count++;
+	});
+};
+
+var repeatOpenUrl = function() {
+	if ( count < 10) {
+		openUrl.call( this);
+		casper.run( repeatOpenUrl);
+	} else {
+		this.echo( 'All done');
+		this.exit();
+	}
+};
+
 casper.start( "https://www.hellowork.go.jp/", function() {
 	this.evaluate( function(){
 		document.querySelector('form').submit();
@@ -19,8 +40,12 @@ casper.then(function(){
 	});
 });
 
-casper.then(function(){
+casper.waitFor( function check() {
+	return this.evaluate( function(){
+		return 'ハローワークインターネットサービス - 求人情報一覧' == document.title;
+	});
+}, function then(){
 	this.capture('submit.png');
 });
 
-casper.run();
+casper.run(repeatOpenUrl);
