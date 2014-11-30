b4c5ecb8bde0b29016457dc67fcddd4fd7a91460
HOMMA Takashi <hommatks@gmail.com>
2014-11-30 09:07:21 +0900

    ファイル名を変更する

--- Jenkins/run-test.cmd
+++ Jenkins/run-test.cmd
@@ -3,4 +3,4 @@ cd "%~dp0..\"
 IF /%1==/ (set CASPERJS=casperjs.exe) else set CASPERJS=%1
 IF /%2==/ (set OUT_FILE=results.xml) else set OUT_FILE=%2
 
-%CASPERJS% test CanSearchForDaniel.js --xunit=%OUT_FILE%
+%CASPERJS% test look-up-Daniel-in-Hellowork.js --xunit=%OUT_FILE%
