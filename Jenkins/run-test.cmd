ecbbf63ebf5648b52a8085b5bab7d042664b58b1
HOMMA Takashi <hommatks@gmail.com>
2014-11-30 09:26:55 +0900

    カレントフォルダを移動しないで実行できるよう変更する

--- Jenkins/run-test.cmd
+++ Jenkins/run-test.cmd
@@ -1,6 +1,7 @@
-cd "%~dp0..\"
+rem CasperJSが、空白含むファイルパス名を実行できないから 8.3 形式にする
+SET BASE_DIR=%~dps0..
 
 IF /%1==/ (set CASPERJS=casperjs.exe) else set CASPERJS=%1
 IF /%2==/ (set OUT_FILE=results.xml) else set OUT_FILE=%2
 
-%CASPERJS% test look-up-Daniel-in-Hellowork.js --xunit=%OUT_FILE%
+%CASPERJS% test %BASE_DIR%\look-up-Daniel-in-Hellowork.js --xunit=%OUT_FILE%
